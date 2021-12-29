0495a694442bfe7d453f255670a4b74b6378b629
zhangqt <vfishv@gmail.com>
2021-12-29 19:55:38 +0800

    fix

--- native-yuv2rgb/src/main/java/com/onzhou/graphic/main/NativeYUV2RGBActivity.java
+++ native-yuv2rgb/src/main/java/com/onzhou/graphic/main/NativeYUV2RGBActivity.java
@@ -2,7 +2,6 @@ package com.onzhou.graphic.main;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.os.AsyncTaskCompat;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
